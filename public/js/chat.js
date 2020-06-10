e10f3227bacaa7c5d9a35b7dc505bfa11fd89853
Daniel Lopez <danielopez07@gmail.com>
2020-06-10 00:38:19 -0300

    Super simple suport for usernames

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,27 +1,33 @@
 
 window.onload = function () {
     var welcomePage = document.getElementById('welcome');
-    var usernameForm = document.getElementById('username-form')
+    var usernameForm = document.getElementById('username-form');
+    var username = document.getElementById('name');
     var chatPage = document.getElementById('chat-page');
+    var backBtn = document.getElementById('back-btn');
     var messages = document.getElementById('messages');
     var chatForm = document.getElementById('chat-msg');
     var msg = document.getElementById('m');
     var socket = io();
-    
-    // welcomePage.classList.add('hidden');
-    // chatPage.classList.add('hidden');
 
     usernameForm.onsubmit = function (e) {
         e.preventDefault();
-        console.log(e);
+        localStorage.setItem('username', username.value);
+        welcomePage.classList.add('hidden');
+        chatPage.classList.remove('hidden');
     }
 
     chatForm.onsubmit = function (e) {
         e.preventDefault();
-        socket.emit('chat message', msg.value);
+        socket.emit('chat message', username.value + ': ' + msg.value);
         msg.value = '';
         return false;
     };
+
+    backBtn.onclick = function (e) {
+        chatPage.classList.add('hidden');
+        welcomePage.classList.remove('hidden');
+    }
 
     socket.on('chat message', function (msg) {
         messages.innerHTML += '<li>' + msg + '</li>';
